7404dfd05379100ec22a02e0108008a5d23df408
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-04-03 22:16:09 +0300

    Tune Query Service

--- query-service/src/main/java/wave/pets/query/MessageController.java
+++ query-service/src/main/java/wave/pets/query/MessageController.java
@@ -1,8 +1,38 @@
 package wave.pets.query;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MessageController {
+import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
+public class MessageController {
+    private final MessageRepository messageRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Message>> getAllMessages() {
+        return ResponseEntity.ok(messageRepository.findAll());
+    }
+
+    @GetMapping("/latest")
+    public ResponseEntity<Message> getLatestMessage() {
+        final var messages = messageRepository.findAll();
+        return ResponseEntity.ok(messages.get(messages.size() - 1));
+    }
+
+    @GetMapping("/one/{message}")
+    public ResponseEntity<String> getMessage(@PathVariable String message) {
+        final var result = messageRepository.findAll()
+                .stream()
+                .map(Message::getMessage)
+                .filter(m -> m.equals(message))
+                .findFirst()
+                .orElse("");
+
+        return ResponseEntity.ok(result);
+    }
 }
--- query-service/src/main/java/wave/pets/query/QueryServiceApplication.java
+++ query-service/src/main/java/wave/pets/query/QueryServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QueryServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(QueryServiceApplication.class, args);
     }
-
 }
