4d809a0dbe254e1c6f80f056b442b8ae7a2a13b5
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-04-09 00:55:47 +0300

    Fix stream data generation

--- data-producer/src/main/java/wave/pets/producer/PetProducer.java
+++ data-producer/src/main/java/wave/pets/producer/PetProducer.java
@@ -4,19 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 import wave.pets.model.Message;
 import wave.pets.repository.MessageRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.UUID;
 
+import static java.time.Duration.ofMillis;
 import static java.util.stream.Stream.concat;
-import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
-import static reactor.core.publisher.Flux.fromStream;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,18 +26,26 @@ public class PetProducer {
     private final List<Message> messages = new ArrayList<>();
     private final MessageRepository messageRepository;
 
-    @Scheduled(fixedRate = 3000)
+    @Scheduled(fixedRate = 250)
     public void readAndUpdate() {
         messages.addAll(concat(messages.stream(), messageRepository.findAll().stream()).distinct().toList());
-        messages.forEach(this::addPetGeo);
     }
 
-    @GetMapping(value = "/geo")
-    public Mono<ServerResponse> addPetGeo(Message message) {
-        return ServerResponse
-                .ok()
-                .contentType(TEXT_EVENT_STREAM)
-                .body(fromStream(Stream.generate(() -> new Message(message.getId(), message.getMessage()))),
-                        Message.class);
+    @GetMapping(value = "/geo/{id}", produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<Message> addPetGeo(@PathVariable UUID id) {
+        Message message = messages.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst()
+                .orElseThrow();
+
+        return Flux.interval(ofMillis(1000))
+                .map(i -> new Message(message.getId(), message.getMessage()))
+                .doOnNext(m -> log.info("Message {} {}", m.getId(), m.getMessage()));
     }
+
+    /*
+       Define some common strategies for Geo and Health data
+       Add method, that by id throws some stream of data according to the strategy
+       Scheduled operation is only for cache management
+     */
 }
