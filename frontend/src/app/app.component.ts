0179e26c487840dd5e79cf4c0ecc9c7344f51544
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-06-07 23:54:33 +0300

    Recreate angular app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { IProduct } from "./models/product";
-import { products as data } from "./data/products";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'frontend';
-
-  products: IProduct[] = data
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,19 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from "./components/user/user.component";
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent, UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
