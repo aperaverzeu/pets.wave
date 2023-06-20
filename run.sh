c10791e7ed85eccc24aafed790bc48087da0f3ca
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-06-20 04:15:30 +0300

    Add runnable script

--- run.sh
+++ run.sh
@@ -9,22 +9,22 @@ docker-compose up -d
 sleep 5
 
 cd gateway
-nohup java -jar gateway-1.0.jar &
+java -jar gateway-1.0.jar &
 
 cd ..
 
 cd data-publisher
-nohup java -jar data-publisher-1.0.jar &
+java -jar data-publisher-1.0.jar &
 
 cd ..
 
 cd data-projector
-nohup java -jar data-projector-1.0.jar &
+java -jar data-projector-1.0.jar &
 
 cd ..
 
 cd query-service
-nohup java -jar query-service-1.0.jar &
+java -jar query-service-1.0.jar &
 
 cd ..
 
