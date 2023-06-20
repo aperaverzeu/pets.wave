122c678f32c85529ed8fc238f5b0f338fc08a928
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2023-06-20 03:57:12 +0300

    Add runnable script

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+mvn clean package -d skipTests
+
+sleep 2
+
+docker-compose up -d
+
+sleep 5
+
+cd gateway
+nohup java -jar gateway-1.0.jar
+
+cd ..
+
+cd data-publisher
+nohup java -jar data-publisher-1.0.jar
+
+cd ..
+
+cd data-projector
+nohup java -jar data-projector-1.0.jar
+
+cd ..
+
+cd query-service
+nohup java -jar query-service-1.0.jar
+
+cd ..
+
+sleep 3
+
+cd frontend
+
+ng serve
